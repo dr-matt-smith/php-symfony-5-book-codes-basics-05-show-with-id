ea18444c23765a80605168d03868229f31c3c3dd
dr-matt-smith <matt.smith@itb.ie>
2020-02-11 09:28:31 +0000

    added files to project

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Student;
+use App\Repository\StudentRepository;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student_list")
+     */
+    public function index()
+    {
+        $studentRepository = new StudentRepository();
+        $students = $studentRepository->findAll();
+
+        $template = 'student/index.html.twig';
+        $args = [
+            'students' => $students
+        ];
+        return $this->render($template, $args);
+    }
+
+    /**
+     * @Route("/student/{id}", name="student_show")
+     */
+    public function show($id)
+    {
+        $studentRepository = new StudentRepository();
+        $student = $studentRepository->find($id);
+
+        // we are assuming $student is not NULL....
+        $template = 'student/show.html.twig';
+        $args = [
+            'student' => $student
+        ];
+        return $this->render($template, $args);
+    }
+
+}
